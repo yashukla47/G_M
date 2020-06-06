63295084b9f580295e28504b4da26eb7c8196614
yashukla47 <48044107+yashukla47@users.noreply.github.com>
2020-06-06 09:50:19 +0530

    Update Vault.sol
    added withdraw to the contract

--- Golden M/Vault.sol
+++ Golden M/Vault.sol
@@ -10,14 +10,8 @@ pragma solidity ^0.5.1;
     Proxy proxy;
     ERC223 erc223;
     
-    mapping (address => bool) isAccountant;
-    
-    
-    address[] public AccountantsArray;
-    event LogAccountantAdded(address indexed _Accountant, address _by);
-    event LogAccountantRemoved(address indexed _Accountant, address _by);
     event LogTransferToken(address _To, uint _Amount, address _by);
-
+    event Logwithdraw(uint _Amount, address _ValutAddress,address _by);
 
     constructor(address _ProxyAddress) public
     {
@@ -29,53 +23,6 @@ pragma solidity ^0.5.1;
         _;
     }
     
-    function getAllAccountant()
-        public
-        view
-        returns(address[] memory)
-    {
-        return AccountantsArray;
-    }
-
-    function addAccountant
-        (
-            address _Accountant
-        )
-            external
-            addressValid(_Accountant)
-        {   
-            require(proxy.isAdmin(msg.sender), "vault:: addAccountant NOT_ADMIN");
-            require(!isAccountant[_Accountant], "vault:: addAccountant ACCOUNT_ALREADY_EXISTS");
-    
-            AccountantsArray.push(_Accountant);
-            isAccountant[_Accountant] = true;
-    
-            emit LogAccountantAdded(_Accountant, msg.sender);
-        }
-    
-    function removeAccountant
-    (
-        address _Accountant
-    ) 
-        external
-        addressValid(_Accountant)
-    {   
-        require(proxy.isAdmin(msg.sender), "vault:: addAccountant NOT_ADMIN");
-        require(isAccountant[_Accountant], "Config::removeAdmin ADMIN_DOES_NOT_EXIST");
-        require(msg.sender != _Accountant, "Config::removeAdmin ADMIN_NOT_AUTHORIZED");
-
-        isAccountant[_Accountant] = false;
-
-        for (uint i = 0; i < AccountantsArray.length - 1; i++) {
-            if (AccountantsArray[i] == _Accountant) {
-                AccountantsArray[i] = AccountantsArray[AccountantsArray.length - 1];
-                AccountantsArray.length -= 1;
-                break;
-            }
-        }  
-
-        emit LogAccountantRemoved(_Accountant, msg.sender);
-    }
     
     function transferTokens 
     (
@@ -83,13 +30,14 @@ pragma solidity ^0.5.1;
         address _TokenAddress, 
         uint _Amount 
     )
-    external 
+    public
     addressValid(_TokenAddress)
     addressValid(_To)
     {
-        require(isAccountant[msg.sender], "Vault:: transferToken ACCOUNTANT_NOT_AUTHORIZED");
-        require(proxy.isTokenAddress(_TokenAddress), "Vault:: transferToken Not_A_Token_Address");
-        require(proxy.transferBalance(_To)>= _Amount, "Vault:: transferToken  NO_Sufficient_Transfer_Balance" );
+        require(proxy.isAccountant(msg.sender), "Vault:: transferToken ACCOUNTANT_NOT_AUTHORIZED");
+        require(proxy.AccountantVaultAddress(msg.sender) == address(this), "Vault:: transferToken ACCOUNTANT_NOT_AUTHORIZED_FOR_THIS_VAULT");
+        require(proxy.isTokenAddress(_TokenAddress), "Vault:: transferToken NOT_A_TOKEN_ADDRESS");
+        require(proxy.transferBalance(_To)>= _Amount, "Vault:: transferToken  NO_SUFFICIENT_TRANSFER_BALANCE" );
         erc223 = ERC223(_TokenAddress);
         erc223.transfer(_To, _Amount);
         proxy.updateTransferBalance(_To, _Amount);
@@ -97,5 +45,28 @@ pragma solidity ^0.5.1;
         emit LogTransferToken(_To, _Amount, msg.sender);
     }
     
+    function withdraw
+    (
+        uint _Amount,
+        address _TokenAddress
+    ) 
+    public
     
- }
+    {
+        require(proxy.isAdmin(msg.sender), "Vault:: withdraw ADMIN_NOT_AUTHORIZED");
+        if (_TokenAddress == address(0x0) )
+        {
+            msg.sender.transfer(_Amount);
+        }
+        if (_TokenAddress != address(0x0))
+        {
+        erc223 = ERC223(_TokenAddress);
+        erc223.transfer(msg.sender, _Amount);
+        }
+        
+        emit Logwithdraw(_Amount, address(this), msg.sender);
+
+    }
+    
+ }    
+    
